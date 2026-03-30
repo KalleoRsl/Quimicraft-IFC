70f98f5a8f80c687ca1da3113049d3c03e69a8a1
KalleoRsl <vavassorikalleo@gmail.com>
2026-03-30 16:04:54 -0300

    Create conexao.php

---
+++ php/conexao.php
@@ -0,0 +1,14 @@
+<?php
+
+$host = "localhost";
+$usuario = "root";
+$senha = "";
+$banco = "quimicraft";
+
+$conexao = mysqli_connect($host, $usuario, $senha, $banco);
+
+if (!$conexao) {
+    die("Erro de conexão: " . mysqli_connect_error());
+}
+
+?>
