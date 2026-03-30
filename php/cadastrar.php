ce8475c1c25e0c69858709b5f176cc4ba8f25482
KalleoRsl <vavassorikalleo@gmail.com>
2026-03-30 16:04:56 -0300

    Create cadastrar.php

---
+++ php/cadastrar.php
@@ -0,0 +1,33 @@
+<?php
+include("conexao.php");
+
+$nome = $_POST['nome'];
+$senha = $_POST['senha'];
+$confirmar = $_POST['confirmar'];
+$turma = $_POST['turma'];
+
+
+if (empty($nome) || empty($senha) || empty($confirmar) || empty($turma)) {
+    echo "<script>alert('Preencha todos os campos!'); window.history.back();</script>";
+    exit();
+}
+
+if ($senha != $confirmar) {
+    echo "<script>alert('As senhas não coincidem!'); window.history.back();</script>";
+    exit();
+}
+
+
+$senhaHash = password_hash($senha, PASSWORD_DEFAULT);
+
+
+$sql = "INSERT INTO usuarios (nome_usuario, senha, id_turma)
+        VALUES ('$nome', '$senhaHash', '$turma')";
+
+if (mysqli_query($conexao, $sql)) {
+    echo "<script>alert('Cadastro realizado com sucesso!'); window.location='../index.html';</script>";
+} else {
+    echo "<script>alert('Erro ao cadastrar usuário!'); window.history.back();</script>";
+}
+
+?>
